4d2448c70f33811c828b5fa0e40dcb9c8035e6ec
Johan Yindou <jyindou@gmail.com>
2023-12-07 13:48:21 +0100

    Je sais pas si ça change qqc

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Vendor\\Omdb\\' => array($baseDir . '/src'),
+    'C:\\wamp64\\www\\omdb' => array($baseDir . '/src'),
 );
